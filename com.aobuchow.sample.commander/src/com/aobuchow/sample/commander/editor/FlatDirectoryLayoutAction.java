ef5e95f4eed86f6e524dd074b95ef4bd271dbd1b
Andrew Obuchowicz <aobuchow@redhat.com>
2020-08-15 01:06:25 -0400

    Flat mode toggle should be file manager dependent
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- com.aobuchow.sample.commander/src/com/aobuchow/sample/commander/editor/FlatDirectoryLayoutAction.java
+++ com.aobuchow.sample.commander/src/com/aobuchow/sample/commander/editor/FlatDirectoryLayoutAction.java
@@ -4,6 +4,7 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 import com.aobuchow.sample.commander.Activator;
+import com.aobuchow.sample.commander.Images;
 
 public class FlatDirectoryLayoutAction extends Action {
 
@@ -14,22 +15,29 @@ public class FlatDirectoryLayoutAction extends Action {
 		this.setEnabled(true);
 		this.setId("com.aobuchow.sample.commander.actions.FlatDirectory"); //$NON-NLS-1$
 		this.setText(Messages.FlatDirectoryLayoutAction_Text);
-		this.setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/view-list_32.png")); //$NON-NLS-1$
+		this.setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, Images.LIST));
 	}
 
 	@Override
 	public void run() {
 		editor.toggleFlatMode();
 		if (editor.isFlatMode()) {
-			this.setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/folder_hero_32.png")); //$NON-NLS-1$
+			this.setImageDescriptor(
+					AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, Images.FOLDER_OUTLINE));
 		} else {
-			this.setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/view-list_32.png")); //$NON-NLS-1$
+			this.setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, Images.LIST));
 		}
 		super.run();
 	}
 
 	public void setEditor(FileManagerEditor targetEditor) {
 		this.editor = targetEditor;
+		if (editor.isFlatMode()) {
+			this.setImageDescriptor(
+					AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, Images.FOLDER_OUTLINE));
+		} else {
+			this.setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, Images.LIST));
+		}
 	}
 
 }
