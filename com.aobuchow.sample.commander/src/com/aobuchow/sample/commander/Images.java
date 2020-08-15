0a1d2571b7f18e87a6bff7ed5daf2c54e231128d
Andrew Obuchowicz <aobuchow@redhat.com>
2020-08-14 20:04:29 -0400

    Add toolbar toggle button for autoplaying audio files
    Fix #39
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

---
+++ com.aobuchow.sample.commander/src/com/aobuchow/sample/commander/Images.java
@@ -0,0 +1,13 @@
+package com.aobuchow.sample.commander;
+
+public interface Images {
+	
+	public static final String AUDIO_MUTE = "icons/volume-off_output.png";
+	
+	public static final String AUDIO_PLAY = "icons/volume-up_output.png";
+	
+	public static final String FOLDER = "icons/folder.png";
+	
+	public static final String AUDIO_FILE = "icons/AudioFile3_16.png";
+
+}
